fb4a01046a9cc6646f0e97809f4acbebb78d8cb6
Jack Karisch <jack.karisch@gmail.com>
2020-10-06 15:52:37 -0500

    Added sunova to 3rd party, and commented

--- python/solarData.py
+++ python/solarData.py
@@ -8,28 +8,32 @@ import time
 
 start = time.time()
 
-NUMMONTHS = 78
+NUMMONTHS = 78 #need to set this for new versions of data (i.e. after 6/30/20)
 
 os.chdir('/home/karisch/programming/projects/solar/python')
 
+#data imports
 pgeRaw = pd.read_csv('./../data/PGE_Interconnected_Applications_Dataset_2020-06-30.csv')
 sceRaw = pd.read_csv('./../data/SCE_Interconnected_Applications_Dataset_2020-06-30.csv')
 sdgeRaw = pd.read_csv('./../data/SDGE_Interconnected_Applications_Dataset_2020-06-30.csv')
 
+#columns to keep; define shortened column names
 cols = ['Technology Type','System Size DC','Storage Size (kW AC)','App Approved Date','Customer Sector','Installer Name','Inverter Manufacturer 1','Third Party Owned','Third Party Name']
 colsShort = ['techType','systemSizeDC','storageSize','appDate','sector','installer','invMan','thirdPartyBool','thirdParty']
 
-allData = pd.concat([pgeRaw[cols][:-1], sceRaw[cols][:-1], sdgeRaw[cols][:-1]])
+allData = pd.concat([pgeRaw[cols][:-1], sceRaw[cols][:-1], sdgeRaw[cols][:-1]]) #concatenate different csv's
 
-allData.columns = colsShort
+allData.columns = colsShort #rename columns
 
-allData = allData[allData['techType'].notna()]
+#clear null entries
+allData = allData[allData['techType'].notna()] 
 allData = allData[allData['invMan'].notna()]
 
 allData.reset_index(inplace=True, drop=True)
 
 solarPvInd = []
 
+#loop to accumulate indices of entries of type 'solar PV' (using regex due to inconsistency in data entries)
 for i in list(allData.index):
 
     checkStr = allData.loc[i, 'techType']
@@ -38,17 +42,20 @@ for i in list(allData.index):
         solarPvInd.append(i)
 
 allData = allData.loc[solarPvInd]
-allData = allData[allData['appDate'] >= '2014-01-01']
+allData = allData[allData['appDate'] >= '2014-01-01'] #only want data 2014 - current
 
 allData.reset_index(inplace=True, drop=True)
 
+#prepare data for installer analysis
 resData = allData[allData['sector'] == 'Residential']
 resData = resData[resData['installer'].notna()]
 resData.reset_index(inplace=True, drop=True)
 
+#prepare data for third party dataset
 tpData = resData[resData['thirdParty'].notna()]
 tpData.reset_index(inplace=True, drop=True)
 
+#prepare data
 simpleMaster(allData, numMonths = NUMMONTHS)
 instMaster(resData, numMonths = NUMMONTHS)
 tpMaster(tpData, numMonths=NUMMONTHS)
--- python/supportingFunctions.py
+++ python/supportingFunctions.py
@@ -2,6 +2,15 @@ import pandas as pd
 import numpy as np
 import re
 
+#### STORAGE SIZE AND SYSTEM SIZE ####
+
+#function to generate subset of dataframe based on regex pattern for a given column
+#inputs:
+#   df: the dataframe to be subset
+#   col: string, name of the column for regex match
+#   pattern: string, regex pattern to match
+#   ignorecase: boolean, if true ignore case in regex match
+#outputs a dataframe that is a subset of input dataset
 def reFilter(df, col, pattern, ignorecase = False):
     ind = []
     for i in list(df.index):
@@ -17,6 +26,9 @@ def reFilter(df, col, pattern, ignorecase = False):
 
     return df
 
+### these functions subset the data for the given inverter manufacturer
+#input is a dataframe to be subset
+#outputs a subset dataframe
 def solarEdgeFilter(df):
     pattern = 'solar ?edge'
 
@@ -73,6 +85,7 @@ def ginlongFilter(df):
 
     return df
 
+#function to generate a subset for all other inverter manufacturers besides those listed above
 def otherFilter(df):
     patterns = ['solar ?edge', 'enphase', 'sma', 'fronius', 'sunpower', 'ap[s ]', 'delta', 'ginlong']
 
@@ -95,6 +108,11 @@ def otherFilter(df):
 
     return df
 
+#function to generate sums of storage and system sizes for the data for a particular subset based on a subset function (defined above)
+#inputs:
+#   df: dataframe to find sums
+#   filterFn: a function that subsets the dataframe before summing
+#outputs a list containing two lists (for storage and system size) of three sums each corresponding to residential, commercial and industrial sectors
 def simpleIndiv(df, filterFn):
     subset = filterFn(df)
 
@@ -112,6 +130,12 @@ def simpleIndiv(df, filterFn):
 
     return [[systemSizeResSum, systemSizeComSum, systemSizeIndSum], [storageSizeResSum, storageSizeComSum, storageSizeIndSum]]
 
+#function to append the monthly sums to the residential, commercial and industrial lists within the storage size and system size dictionaries
+#inputs:
+#   dict: dictionary (either storage or system size) holding the monthly time series lists for each sector
+#   sums: list with three items for the three sectors
+#   key: string, key of company of the sums
+#outputs the dictionary with the new sums appended
 def dictAppend(dict, sums, key):
     dict[key]['residential'].append(sums[0])
     dict[key]['commercial'].append(sums[1])
@@ -119,45 +143,57 @@ def dictAppend(dict, sums, key):
 
     return dict
 
+#function that generates a monthly time series csv from a storage or system size dictionary
+#inputs:
+#   dict: a dictionary with three sector time series lists for each inverter manufacturer
+#   filename: string for name of file to be output
 def simpleCsv(dict, filename):
-    mans = ['solaredge','enphase','sma','fronius','sunpower','aps','delta','ginlong','other']
-    sectors = ['residential', 'commercial', 'industrial']
+    mans = ['solaredge','enphase','sma','fronius','sunpower','aps','delta','ginlong','other'] #inverter manufacturers
+    sectors = ['residential', 'commercial', 'industrial'] #market sectors
 
     startingDate = pd.to_datetime('2013-12-31')
 
     index = []
 
+    #loop to generate index names (months) for the output csv
     for i in range(len(dict[mans[0]][sectors[0]])):
         index.append(startingDate + pd.tseries.offsets.MonthEnd(i + 1))
 
     outDF = pd.DataFrame(index = index)
 
+    #loop to populate dataframe with time series data for each inverter manufacturer for each sector
     for man in mans:
         for sector in sectors:
-            col = man + '_' + sector
+            col = man + '_' + sector #column names based on manufacturer and sector separated by underscore
 
             outDF[col] = dict[man][sector]
 
-    path = './../outputs/' + filename
+    path = './../outputs/' + filename #output in 'outputs' folder
 
     outDF.to_csv(path)
 
+#function to generate the system and storage size time series csv's from a given full dataset
+#inputs:
+#   df: full solar dataset
+#   numMonths: number of months after January 2014
 def simpleMaster(df, numMonths):
-    df = df[df['systemSizeDC'].notna()]
-    df['storageSize'].fillna(0, inplace = True)
+    df = df[df['systemSizeDC'].notna()] 
+    df['storageSize'].fillna(0, inplace = True) #fill null values in storage size with zeros
 
     startingDate = pd.to_datetime('2013-12-31')
 
-    mans = ['solaredge','enphase','sma','fronius','sunpower','aps','delta','ginlong','other']
-    fns = [solarEdgeFilter, enphaseFilter, smaFilter, froniusFilter, sunPowerFilter, apsFilter, deltaFilter, ginlongFilter, otherFilter]
+    mans = ['solaredge','enphase','sma','fronius','sunpower','aps','delta','ginlong','other'] #inverter manufacturers
+    fns = [solarEdgeFilter, enphaseFilter, smaFilter, froniusFilter, sunPowerFilter, apsFilter, deltaFilter, ginlongFilter, otherFilter] #subset functions corresponding to each inverter manufacturer
 
     systemSizeDict = {}
     storageSizeDict = {}
 
+    #loop to create storage and system size dictionaries with keys for each manufacturer
     for man in mans:
         systemSizeDict[man] = {'residential': [], 'commercial': [], 'industrial': []}
         storageSizeDict[man] = {'residential': [], 'commercial': [], 'industrial': []}
- 
+    
+    #loop to subset the data by month
     for i in range(numMonths):
         priorMonth = startingDate + pd.tseries.offsets.MonthEnd(i)
         activeMonth = startingDate + pd.tseries.offsets.MonthEnd(i + 1)
@@ -165,15 +201,19 @@ def simpleMaster(df, numMonths):
         activeSubset = df[pd.to_datetime(df['appDate']) > priorMonth]
         activeSubset = activeSubset[pd.to_datetime(activeSubset['appDate']) <= activeMonth]
 
+        #loop to find monthly sums for each manufacturer and sector, and populate the dictionaries
         for j in range(len(mans)):
             [systemSizeSums, storageSizeSums] = simpleIndiv(activeSubset, fns[j])
             systemSizeDict = dictAppend(systemSizeDict, systemSizeSums, mans[j])
             storageSizeDict = dictAppend(storageSizeDict, storageSizeSums, mans[j])
-        
+
+    #generate csv's  
     simpleCsv(systemSizeDict, 'systemSize.csv')
     simpleCsv(storageSizeDict, 'storageSize.csv')
 
-##################################################################################
+#### INSTALLER CSV ####
+
+#functions to filter data for the given installer (operate like manufacturer filter functions defined above)
 
 def sunrunFilter(df):
     pattern = 'sun ?run'
@@ -267,12 +307,14 @@ def otherInstFilter(df):
 
     return df
 
+#like dictAppend function defined above, but using installers -> manufacturers instead of manufacturers -> sector
 def instDictAppend(dict, sums, instKey, manKeys):
     for i in range(len(sums)):
         dict[instKey][manKeys[i]].append(sums[i])
 
     return dict
 
+#like simpleIndiv function defined above, but using installers -> manufacturers instead of manufacturers -> sector
 def instIndiv(df, instFilterFn, manFilterFns):
     subset = instFilterFn(df)
 
@@ -285,6 +327,7 @@ def instIndiv(df, instFilterFn, manFilterFns):
 
     return sums
 
+#like simpleCsv function defined above, but using installers -> manufacturers instead of manufacturers -> sector
 def instCsv(dict, filename):
     installers = ['sunrun','vivint','freedomforever','solarcity','sunpower','petersen','sungevity','semper','solcius','brightplanet','other']
     mans = ['solaredge','enphase','sma','fronius','sunpower','aps','delta','ginlong','other']
@@ -308,6 +351,7 @@ def instCsv(dict, filename):
 
     outDF.to_csv(path)
 
+#like simpleMaster function defined above, but using installers -> manufacturers instead of manufacturers -> sector
 def instMaster(df, numMonths):
     df = df[df['installer'].notna()]
 
@@ -338,7 +382,9 @@ def instMaster(df, numMonths):
 
     instCsv(outDict, 'installers.csv')
 
-##############################################################################
+#### THIRD PARTY OWNER CSV ####
+
+#these functions are just like the installer functions above, but using third parties instead of installers
 
 def sunrunTpFilter(df):
     pattern = 'sun ?run'
@@ -375,8 +421,8 @@ def sunpowerTpFilter(df):
 
     return df
 
-def petersenTpFilter(df):
-    pattern = 'peters[eo]n ?dean'
+def sunovaTpFilter(df):
+    pattern = 'sunova'
 
     df = reFilter(df, 'thirdParty', pattern, ignorecase = True)
 
@@ -411,7 +457,7 @@ def brightplanetTpFilter(df):
     return df
 
 def otherTpFilter(df):
-    patterns = ['sun ?run','vivint','freed(om|in) ?forever','solar ?city|tesla','^sun ?power','peters[eo]n ?dean','sungevity','semper ?solaris','solcius','bright ?planet']
+    patterns = ['sun ?run','vivint','freed(om|in) ?forever','solar ?city|tesla','^sun ?power','sunova','sungevity','semper ?solaris','solcius','bright ?planet']
 
     otherInd = []
 
@@ -432,15 +478,38 @@ def otherTpFilter(df):
 
     return df
 
+def tpCsv(dict, filename):
+    installers = ['sunrun','vivint','freedomforever','solarcity','sunpower','sunova','sungevity','semper','solcius','brightplanet','other']
+    mans = ['solaredge','enphase','sma','fronius','sunpower','aps','delta','ginlong','other']
+
+    startingDate = pd.to_datetime('2013-12-31')
+
+    index = []
+
+    for i in range(len(dict[installers[0]][mans[0]])):
+        index.append(startingDate + pd.tseries.offsets.MonthEnd(i + 1))
+
+    outDF = pd.DataFrame(index = index)
+
+    for installer in installers:
+        for man in mans:
+            col = installer + '_' + man
+
+            outDF[col] = dict[installer][man]
+
+    path = './../outputs/' + filename
+
+    outDF.to_csv(path)
+
 def tpMaster(df, numMonths):
     df = df[df['installer'].notna()]
 
     startingDate = pd.to_datetime('2013-12-31')
 
-    installers = ['sunrun','vivint','freedomforever','solarcity','sunpower','petersen','sungevity','semper','solcius','brightplanet','other']
+    installers = ['sunrun','vivint','freedomforever','solarcity','sunpower','sunova','sungevity','semper','solcius','brightplanet','other']
     mans = ['solaredge','enphase','sma','fronius','sunpower','aps','delta','ginlong','other']
 
-    instFns = [sunrunTpFilter,vivintTpFilter,freedomforeverTpFilter,solarcityTpFilter,sunpowerTpFilter,petersenTpFilter,sungevityTpFilter,semperTpFilter,solciusTpFilter,brightplanetTpFilter,otherTpFilter]
+    instFns = [sunrunTpFilter,vivintTpFilter,freedomforeverTpFilter,solarcityTpFilter,sunpowerTpFilter,sunovaTpFilter,sungevityTpFilter,semperTpFilter,solciusTpFilter,brightplanetTpFilter,otherTpFilter]
     manFns = [solarEdgeFilter, enphaseFilter, smaFilter, froniusFilter, sunPowerFilter, apsFilter, deltaFilter, ginlongFilter, otherFilter]
 
     outDict = {}
@@ -460,4 +529,4 @@ def tpMaster(df, numMonths):
             activeSums = instIndiv(activeSubset, instFns[j], manFns)
             outDict = instDictAppend(outDict, activeSums, installers[j], mans)
 
-    instCsv(outDict, 'tpinstallers.csv')
+    tpCsv(outDict, 'tpinstallers.csv')
